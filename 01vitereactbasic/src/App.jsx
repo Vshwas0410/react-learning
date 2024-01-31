63438c54f5e465aa1f6b224d325f8acaf5a2ffbe
Vshwas0410 <vishwassingh0441097@gmail.com>
2024-02-01 00:30:09 +0530

    nn

---
+++ 01vitereactbasic/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Chai from "./chai"
+
+
+function App() {
+
+
+  return (
+    <>
+   <Chai/>
+   <h>my Name is Vishwas</h>
+   <p>JSX expressions must have one parent element. So write all 
+    the tag with empty tag inside that You can write as many tag as possible.</p>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereactbasic/src/chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+    <h1>me learning react with dedication</h1>
+    )
+}
+export default Chai
